c4f21b255a318ac866e323553b59d3cb8f5c07c8
Santiago Alfonso Rodriguez <santiago_alfonsoro@soy.sena.edu.co>
2025-05-13 18:52:13 -0500

    función con parámetros y sin retorno implementada

---
+++ src/PrintSum.java
@@ -0,0 +1,10 @@
+public class PrintSum {
+    public static void main(String[] args) {
+        mostrarSuma(8, 12);
+    }
+
+    public static void mostrarSuma(int a, int b) {
+        int suma = a + b;
+        System.out.println("La suma es: " + suma);
+    }
+}
