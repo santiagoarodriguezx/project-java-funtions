1c1ec98304e0026d5cda6fec64050beea97c0a2d
Santiago Alfonso Rodriguez <santiago_alfonsoro@soy.sena.edu.co>
2025-05-13 18:48:30 -0500

    función sin parámetros y sin retorno implementada

---
+++ src/VoidFunction.java
@@ -0,0 +1,9 @@
+public class VoidFunction {
+    public static void main(String[] args) {
+        saludar(); // Llamamos la función
+    }
+
+    public static void saludar() {
+        System.out.println("¡Hola, mundo!");
+    }
+}
