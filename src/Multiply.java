a03f3ba173f433a3015ed0c84f1252090e4f0a59
Santiago Alfonso Rodriguez <santiago_alfonsoro@soy.sena.edu.co>
2025-05-13 19:01:24 -0500

    función con parámetros y con retorno implementada

---
+++ src/Multiply.java
@@ -0,0 +1,10 @@
+public class Multiply {
+    public static void main(String[] args) {
+        int resultado = multiplicar(4, 6);
+        System.out.println("Resultado: " + resultado);
+    }
+
+    public static int multiplicar(int a, int b) {
+        return a * b;
+    }
+}
